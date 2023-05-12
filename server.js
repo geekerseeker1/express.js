2f4dd10a05b68c2bbde2ee6a2f6237b9172c84ef
Pradeep sharma <108931025+geekerseeker1@users.noreply.github.com>
2023-05-12 10:20:32 +0545

    Add files via upload

--- server.js
+++ server.js
@@ -2,28 +2,37 @@ const express = require('express');
 const app = express();
 const port = 3000; // You can change the port number if needed
 
-// Set the views directory and template engine
-app.set('views', './views');
-app.set('view engine', 'ejs'); // You can choose a different template engine if you prefer
+// JSON data
+const products = [
+  {
+    "id": 1,
+    "name": "Noodles",
+    "price": 20
+  },
+  {
+    "id": 2,
+    "name": "Pizza",
+    "price": 400
+  },
+  {
+    "id": 3,
+    "name": "Cookies",
+    "price": 50
+  }
+];
 
-// Middleware to parse request bodies
-app.use(express.urlencoded({ extended: true }));
-
-// Routes
-app.get('/', (req, res) => {
-  res.render('registration');
+// API to list all products
+app.get('/products', (req, res) => {
+  res.json(products);
 });
 
-app.post('/register', (req, res) => {
-  const { firstname, lastname, username } = req.body;
+// API to fetch a product with the highest price
+app.get('/products/highest-price', (req, res) => {
+  let highestPriceProduct = products.reduce((prev, current) => {
+    return (prev.price > current.price) ? prev : current;
+  });
 
-  // Save user data to a database or perform necessary actions
-  // You can save the profile picture file to a folder and store its path in the database
-
-  res.redirect('/user/' + username);
+  res.json(highestPriceProduct);
 });
 
-app.get('/user/:username', (req, res) => {
-  const { username } = req.params;
-
-  // Fetch
+// API to fetch the sum of prices of all products
